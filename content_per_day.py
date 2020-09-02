49d648803016df0f224c7fd1b17b14d126dcb471
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2020-09-02 09:17:02 +0200

    content 2020

--- content_per_day.py
+++ content_per_day.py
@@ -16,13 +16,17 @@ content_per_day = {}
 
 # lundi
 content_per_day[0] = """
+* 6h40-7h14 - gare - train
 * 8h-9h50 - 213 - Tale NSI
 * 10h00-11h50 - 307 - 1ere NSI
+* 12h14-12h47 - gare - train
 """
 
 # mardi
 content_per_day[1] = """
+* 12h40-13h14 - gare - train
 * 13h40-15h30 - 213 - Tale NSI
+* 15h40-16h14 - gare - train
 """
 
 # mercredi
@@ -31,16 +35,20 @@ content_per_day[2] = """
 
 # jeudi
 content_per_day[3] = """
+* 6h40-7h14 - gare - train
 * 8h-9h50 - 213 - 1ere NSI g1
 * 10h-11h50 - 213 - 1ere NSI g2
 * 11h50-12h40 - 301 - Aéro2 g1
 * 13h40-14h35 - 301 - Aéro2 g2
+* 15h14-15h47 - gare - train
 """
 
 # vendredi
 content_per_day[4] = """
+* 6h40-7h14 - gare - train
 * 8h-9h50 - 213 - Tale NSI
-* 10h00-11h50 - 301 - Aéro
+* 10h00-10h55 - 301 - Aéro
+* 11h14-11h47 - gare - train
 """
 
 # samedi
