6ccf5dc1e1d78992198a1df2c272155ae7ba4d88
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2021-08-24 09:41:58 +0200

    bug: calendar weeks offset by one

--- cahier_texte_generator.py
+++ cahier_texte_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
+"""
 ---
 title: "Générateur de Cahier de texte"
 author: "qkzk"
@@ -9,44 +9,43 @@ version: "1.1"
 date: "2019/08/02"
 ---
 
-'''
+"""
 
+import calendar
 import datetime
-import pathlib
 import os
+import pathlib
 import sys
+from calendar import HTMLCalendar
 from datetime import timedelta
 from pprint import pprint
 
-import calendar
-from calendar import HTMLCalendar
-
 from content_per_day import content_per_day
 
-year = 2019
+year = 2021
 start_week = None
 liste_periode = range(1, 6)
 dic_fin_periodes = {
-    0: "31/08/2020", # pre rentrée
-    1: "02/11/2020", # rentrée vacances Toussaint
-    2: "04/01/2021", # rentrée vacances Noël
-    3: "08/03/2021", # rentrée vacances d'hiver
-    4: "10/05/2021", # rentrée vacances Printemps
-    5: "06/07/2021"  # fin d'année scolaire
+    0: "1/09/2021",  # pre rentrée
+    1: "08/11/2021",  # rentrée vacances Toussaint
+    2: "03/01/2022",  # rentrée vacances Noël
+    3: "21/02/2022",  # rentrée vacances d'hiver
+    4: "25/04/2022",  # rentrée vacances Printemps
+    5: "07/07/2022",  # fin d'année scolaire
 }
 
 # constants
 TEXT_COLORS = {
-    "PURPLE": '\033[95m',
-    "CYAN": '\033[96m',
-    "DARKCYAN": '\033[36m',
-    "BLUE": '\033[94m',
-    "GREEN": '\033[92m',
-    "YELLOW": '\033[93m',
-    "RED": '\033[91m',
-    "BOLD": '\033[1m',
-    "UNDERLINE": '\033[4m',
-    "END": '\033[0m',
+    "PURPLE": "\033[95m",
+    "CYAN": "\033[96m",
+    "DARKCYAN": "\033[36m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "YELLOW": "\033[93m",
+    "RED": "\033[91m",
+    "BOLD": "\033[1m",
+    "UNDERLINE": "\033[4m",
+    "END": "\033[0m",
 }
 
 
@@ -81,43 +80,43 @@ WELCOME_BANNER = """Bienvenue dans ...
 
 """
 
-warning_periods_year = '''
+warning_periods_year = """
 Generation du cahier de texte de l'année {}.
 
 Attention,
 il faut modifier les dates des périodes dans les sources !
 Les périodes en mémoire sont sont :
-'''
-warning_emploi_du_temps = '''
+"""
+warning_emploi_du_temps = """
 Attention, vous pouvez aussi modifier l'emploi du temps de chaque journée
 dans les sources.
 
 Il faut modifier la variable "content_per_day"
 
 Pour l'instant elle contient :
-'''
+"""
 
 traduction = {
     # on pourrait utiliser des locales et traduire automatiquement...
-    "Monday":       "Lundi",
-    "Tuesday":      "Mardi",
-    "Wednesday":    "Mercredi",
-    "Thursday":     "Jeudi",
-    "Friday":       "Vendredi",
-    "Saturday":     "Samedi",
-    "Sunday":       "Dimanche",
-    "January":      "janvier",
-    "February":     "février",
-    "March":        "mars",
-    "April":        "avril",
-    "May":          "mai",
-    "June":         "juin",
-    "July":         "juillet",
-    "August":       "août",
-    "September":    "septembre",
-    "October":      "octobre",
-    "November":     "novembre",
-    "December":     "décembre",
+    "Monday": "Lundi",
+    "Tuesday": "Mardi",
+    "Wednesday": "Mercredi",
+    "Thursday": "Jeudi",
+    "Friday": "Vendredi",
+    "Saturday": "Samedi",
+    "Sunday": "Dimanche",
+    "January": "janvier",
+    "February": "février",
+    "March": "mars",
+    "April": "avril",
+    "May": "mai",
+    "June": "juin",
+    "July": "juillet",
+    "August": "août",
+    "September": "septembre",
+    "October": "octobre",
+    "November": "novembre",
+    "December": "décembre",
 }
 
 
@@ -125,28 +124,29 @@ traduction = {
 liste_fin_periode = []
 
 for period_nb, string_fin_periode in dic_fin_periodes.items():
-    date_fin_periode = datetime.datetime.strptime(
-        string_fin_periode, '%d/%m/%Y'
-    )
+    date_fin_periode = datetime.datetime.strptime(string_fin_periode, "%d/%m/%Y")
     liste_fin_periode.append(date_fin_periode)
+
+print("liste fin de période")
+print(liste_fin_periode)
+
 
 # dic_semaine_periode = {}
 
 
 # FONCTIONS
 
+
 def get_start_and_end_date_from_calendar_week(year, calendar_week):
-    '''
+    """
     Renvoie une liste de jours d'une semaine d'une année
 
     @param year: (int) numero de l'année : 2019
     @param calendar_week: (int) numero de la semaine : 36
     @return: (list of date) la liste des dates de la semaine du lundi au
         dimanche
-    '''
-    monday = datetime.datetime.strptime(
-        f'{year}-{calendar_week}-1', "%Y-%W-%w"
-    ).date()
+    """
+    monday = datetime.datetime.strptime(f"{year}-{calendar_week}-1", "%Y-%W-%w").date()
 
     list_days = []
     for d in range(7):
@@ -156,21 +156,21 @@ def get_start_and_end_date_from_calendar_week(year, calendar_week):
 
 
 def create_file_content(sem):
-    '''
+    """
     Renvoie le contenu d'un fichier semaine.
 
     Le fichier est formaté markdown et on le remplit.
 
     @param sem: (int) numéro de semaine
     @return : (str) la chaîne de caractères qui est écrite dans le fichier
-    '''
+    """
     current_year = year
     # on choisit la bonne année courante
     if int(sem) < 30:
         current_year = current_year + 1
 
     # on récupère la liste des dates de la semaine
-    list_days = get_start_and_end_date_from_calendar_week(current_year, sem - 1)
+    list_days = get_start_and_end_date_from_calendar_week(current_year, sem)
 
     # on formate toutes les dates pour énumérer plus facilement
     list_string_day = list(map(format_string_jour, list_days))
@@ -190,24 +190,20 @@ def create_file_content(sem):
 
 
 def format_string_jour(day):
-    '''
+    """
     Traduit une date au format français pour être écrite dans le fichier
 
     @param day: (date) date d'un jour
     @return : (str) le format d'un jour : Lundi 03 Septembre
-    '''
+    """
     day_of_the_week = traduction[day.strftime("%A")]
     month = traduction[day.strftime("%B")]
-    string_day = "{0} {1} {2}".format(
-        day_of_the_week,
-        day.strftime("%d"),
-        month
-    )
+    string_day = "{0} {1} {2}".format(day_of_the_week, day.strftime("%d"), month)
     return string_day
 
 
 def create_md_file(period_nb, week_number):
-    '''
+    """
     Crée le fichier .md de chaque semaine et le remplit
     Les dossiers existent déjà à cette étape, on se contente de les remplir
     de fichier et de remplir les fichiers.
@@ -216,38 +212,36 @@ def create_md_file(period_nb, week_number):
     @param week_number: (int) numero de semaine : 36
     @return : (None)
     SE : crée un fichier et écrit dedans
-    '''
+    """
 
     # 0 ---> ./calendrier/periode_1
     path = default_path_md + str(period_nb + 1)
     # 36 ---> semaine_36.md
     filename = default_file_name.format(week_number)
-    with open(os.path.join(path, filename), 'w+') as f:
+    with open(os.path.join(path, filename), "w+") as f:
         content = create_file_content(week_number)
         f.write(content)
         return
 
 
 def create_cahier_texte(start_week=None):
-    '''
+    """
     Fonction principale qui crée les fichiers et les remplit pour chaque
     période et chaque semaine de chaque période.
 
-    On parcours chaque période de la liste des périodes.
+    On parcourt chaque période de la liste des périodes.
     On crée les sous dossiers
 
     @param:
     @return None
     SE : plante en fin de travail, quand tout est fait, donc c'est pas important
-    '''
+    """
     # on cree le dossier de l'année
     pathlib.Path(default_path_year).mkdir(parents=True, exist_ok=True)
 
     # on crée les dossiers de période
     for periode in liste_periode:
-        pathlib.Path(
-            default_path_md + str(periode)).mkdir(parents=True, exist_ok=True
-                                                  )
+        pathlib.Path(default_path_md + str(periode)).mkdir(parents=True, exist_ok=True)
 
     # on peuple les dossiers de période
     for period_nb, v in dic_fin_periodes.items():
@@ -273,23 +267,21 @@ def create_cahier_texte(start_week=None):
                     semaine_debut_periode = start_week
 
             # on affiche un peu pour se repérer et voir l'avancée
-            print('periode {}'.format(period_nb + 1), end=' - ')
-            print("semaines {} jusque {}".format(
-                semaine_debut_periode,
-                semaine_fin_periode)
+            print("periode {}".format(period_nb + 1), end=" - ")
+            print(
+                "semaines {} jusque {}".format(
+                    semaine_debut_periode, semaine_fin_periode
+                )
             )
             if semaine_debut_periode < semaine_fin_periode:
                 # période normale, pas de changement d'année civile
-                for sem_nbr in range(
-                    semaine_debut_periode,
-                    semaine_fin_periode
-                ):
+                for sem_nbr in range(semaine_debut_periode, semaine_fin_periode):
                     create_md_file(period_nb, sem_nbr)
             else:
                 # changement d'année civile en cours de période
                 for sem_nbr in range(semaine_debut_periode, 53):
                     create_md_file(period_nb, sem_nbr)
-                for sem in range(1, semaine_fin_periode):
+                for sem_nbr in range(1, semaine_fin_periode):
                     create_md_file(period_nb, sem_nbr)
         except IndexError as e:
             print("\nCalendrier généré correctement, fin du programme")
@@ -298,12 +290,12 @@ def create_cahier_texte(start_week=None):
 
 
 class EventsMonthCalendar(HTMLCalendar):
-    '''
+    """
     Classe qui étend HTMLCalendar en formattant les jours
     Utilisée pour générer une table d'un mois dont les jours pointent
     vers une page de la semaine.
     Les pages étant rangées dans des sous dossiers par période
-    '''
+    """
 
     def __init__(self, year, month, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -313,30 +305,30 @@ class EventsMonthCalendar(HTMLCalendar):
         self.end_url = ".md"
 
     def which_day(self, day):
-        '''
+        """
         Renvoie la date du jour en question
         @param day: (int) le jour
         @return: (datetime) datetime du jour
-        '''
+        """
         return datetime.datetime(self.year, self.month, day)
 
     def which_period(self, day):
-        '''
+        """
         Renvoie la période du jour en question
         @param day: (int) le numéro du jour
         @return: (int) le numéro de la période
-        '''
+        """
         theday = self.which_day(day)
         for nb_period, date_fin_period in enumerate(liste_fin_periode):
             if theday < date_fin_period:
                 return nb_period
 
     def which_weeknumber(self, day):
-        '''
+        """
         Renvoie le numéro de la semaine correspondante
         @param day: (int) le numéro du jour
         @return: (int) le numéro de la semaine
-        '''
+        """
         theday = self.which_day(day)
         return theday.isocalendar()[1]
 
@@ -353,8 +345,9 @@ class EventsMonthCalendar(HTMLCalendar):
         TODO modifier le nom de l'auteur et du repo pour être plus générique
         """
         school_year = self.year if nb_week > 30 else self.year - 1
-        middle_url = str(school_year) + "/periode_" + str(nb_period) + \
-            "/semaine_" + str(nb_week)
+        middle_url = (
+            str(school_year) + "/periode_" + str(nb_period) + "/semaine_" + str(nb_week)
+        )
         return self.start_url + middle_url + self.end_url
 
     def formatday(self, day, weekday):
@@ -375,41 +368,39 @@ class EventsMonthCalendar(HTMLCalendar):
 
 
 def generateMonthes():
-    '''
+    """
     Génère du contenu HTML dans une string
     Pour être affiché dans github
 
     @return: (str) une page HTML avec un calendrier de l'année scolaire.
         Chaque jour est un lien vers une page github
-    '''
-    html_string = ''
+    """
+    html_string = ""
 
     for month in range(9, 13):
         html_string += "\n" * 3
-        html_string += EventsMonthCalendar(year, month).formatmonth(
-            year, month)
+        html_string += EventsMonthCalendar(year, month).formatmonth(year, month)
 
     for month in range(1, 7):
         html_string += "\n" * 3
-        html_string += EventsMonthCalendar(year + 1, month).formatmonth(
-            year + 1, month)
+        html_string += EventsMonthCalendar(year + 1, month).formatmonth(year + 1, month)
 
     return html_string
 
 
 def write_html_monthes():
-    '''
+    """
     Ecrit le fichier README.md avec le contenu HTML à l'adresse :
     ./calendrier/2019/README.md
 
     Utilise les méthodes generateMonthes et la classe EventsMonthCalendar
     @return: (None)
     SE: crée un fichier écrit dedans.
-    '''
+    """
     # 0 ---> ./calendrier/2019
     path = default_path_year
-    filename = 'README.md'
-    with open(os.path.join(path, filename), 'w+') as f:
+    filename = "README.md"
+    with open(os.path.join(path, filename), "w+") as f:
         content = generateMonthes()
         f.write(content)
         return
@@ -419,7 +410,7 @@ def color_text(text, color="BOLD"):
     return TEXT_COLORS[color] + text + TEXT_COLORS["END"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(color_text(WELCOME_BANNER, "DARKCYAN"))
     args = sys.argv
     if len(args) > 1:
@@ -430,14 +421,20 @@ if __name__ == '__main__':
                 start_week = int(args[2])
             print(warning_periods_year.format(year))
             pprint(dic_fin_periodes)
-            reponse_annee = input(color_text(
-                color_text("Voulez-vous continuer ? (y/N) : ", "BOLD"), "RED"))
+            reponse_annee = input(
+                color_text(
+                    color_text("Voulez-vous continuer ? (y/N) : ", "BOLD"), "RED"
+                )
+            )
             if not reponse_annee == "y":
                 exit("Fin du programme, aucun calendrier n'a été généré")
             print(warning_emploi_du_temps)
             pprint(content_per_day)
-            reponse_edt = input(color_text(
-                color_text("Voulez-vous continuer ? (y/N) : ", "BOLD"), "RED"))
+            reponse_edt = input(
+                color_text(
+                    color_text("Voulez-vous continuer ? (y/N) : ", "BOLD"), "RED"
+                )
+            )
             if not reponse_edt == "y":
                 exit("Fin du programme, aucun calendrier n'a été généré")
             print()
@@ -446,8 +443,7 @@ if __name__ == '__main__':
             print("L'argument doit être une année, par exemple : 2019")
             raise e
     else:
-        print("Aucune année fournie, on utilise l'année {} comme exemple"
-              .format(year))
+        print("Aucune année fournie, on utilise l'année {} comme exemple".format(year))
     # l'user a tout compris on peut créer 45 fichiers :)
     default_path_year = "./calendrier/{}/".format(year)
     default_path_md = "./calendrier/{}/periode_".format(year)
--- content_per_day.py
+++ content_per_day.py
@@ -1,4 +1,4 @@
-'''
+"""
 content_per_day = {
     0: "\n* 8h-8h55 - s213\n* 8h55-9h50 - s215\n* 10h-10h55 - s104\n",
     1: "\n* 8h-8h55 - s213\n* 8h55-9h50 - s215\n* 10h-10h55 - s104\n",
@@ -9,52 +9,70 @@ content_per_day = {
     6: "\n",
 }
 
-'''
+"""
 
 content_per_day = {}
 
 
 # lundi
-content_per_day[0] = """
+content_per_day[
+    0
+] = """
 * 6h40-7h14 - gare - train
-* 8h-9h50 - 213 - Tale NSI
-* 10h00-11h50 - 307 - 1ere NSI
-* 12h14-12h47 - gare - train
+* 8h-9h50 - 213 - 1ere NSI
+* 10h00-11h50 - 213 - tale NSI
+* 13h10-14h35 - 213 - 2nde 8 g2
+* 14h35-15h55 - 213 - 2nde 8 g1
+* 16h14-16h47 - gare - train
 """
 
 # mardi
-content_per_day[1] = """
+content_per_day[
+    1
+] = """
 * 12h40-13h14 - gare - train
 * 13h40-15h30 - 213 - Tale NSI
 * 15h40-16h14 - gare - train
 """
 
 # mercredi
-content_per_day[2] = """
+content_per_day[
+    2
+] = """
 """
 
 # jeudi
-content_per_day[3] = """
+content_per_day[
+    3
+] = """
 * 6h40-7h14 - gare - train
 * 8h-9h50 - 213 - 1ere NSI g1
 * 10h-11h50 - 213 - 1ere NSI g2
-* 11h50-12h40 - 301 - Aéro2 g1
-* 13h40-14h35 - 301 - Aéro2 g2
-* 15h14-15h47 - gare - train
+* 11h50-12h45 - 213 - 2nde8 Orientation 1-2
+* 14h35-15h30 - 206 - 2nde8
+* 15h40-16h35 - 217 - 2nde8 AP
+* 16h51-17h37 - gare - train
 """
 
 # vendredi
-content_per_day[4] = """
+content_per_day[
+    4
+] = """
 * 6h40-7h14 - gare - train
 * 8h-9h50 - 213 - Tale NSI
-* 10h00-10h55 - 301 - Aéro
-* 11h14-11h47 - gare - train
+* 10h55-11h50 - 106 - 2nde8
+* 11h50-12h45 - 217 - 2nde8 Q1
+* 13h21-14h07 - gare - train
 """
 
 # samedi
-content_per_day[5] = """
+content_per_day[
+    5
+] = """
 """
 
 # dimanche
-content_per_day[6] = """
+content_per_day[
+    6
+] = """
 """
